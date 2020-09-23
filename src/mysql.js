2621e43df398d500ba6100375d1ac4703577302b
Valentina Paniza <valentinapaniza93@gmail.com>
2020-09-23 20:32:52 -0300

    .

--- src/mysql.js
+++ src/mysql.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const database = "delila_db";
+const database = "delilah_db";
 const user = "username";
 const host = "192.168.64.2";
 const password = "password";
